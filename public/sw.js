125a95f50b8ecdcebe76007cd97cafeeac8a6714
joanakar3dmoon <joanlazaro83@gmail.com>
2026-07-19 12:22:45 +0200

    feat(pwa): add public/sw.js

---
+++ public/sw.js
@@ -0,0 +1,14 @@
+// InverGrow Service Worker
+const CACHE = 'invergrow-v1';
+const ASSETS = ['/', '/index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
